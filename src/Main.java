ae885040b1b1b9bf0a6fe7433ed6e0ae255b5e6a
JohnKill87 <yankin.ilja2012@yandex.ru>
2023-05-02 18:53:05 +0300

    Решение домашнего задания 10 (строки)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        String firstName = "Имя";
+        firstName = "Ivan";
+        String middleName = "Отчество";
+        middleName = "Ivanovich";
+        String lastName = "Фамилия";
+        lastName = "Ivanov";
+        String fullName = "Фамилия Имя Отчество";
+        fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        String fullName = "Ivanov Ivan Ivanovich";
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        String fullName = "Иванов Семён Семёнович";
+        String fullNameTrue = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullNameTrue);
     }
 }
